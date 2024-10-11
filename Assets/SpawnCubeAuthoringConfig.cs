13b4b961afbbaa33cbbec37bd99272c5b54fe531
JohanAbrahamssonHis <118519456+JohanAbrahamssonHis@users.noreply.github.com>
2024-10-11 16:36:42 +0200

    Spawning objects Completed

---
+++ Assets/SpawnCubeAuthoringConfig.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class SpawnCubeAuthoringConfig : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    public int spawnAmount;
+
+    public class Baker : Baker<SpawnCubeAuthoringConfig>
+    {
+        public override void Bake(SpawnCubeAuthoringConfig authoring)
+        {
+            Entity entity = GetEntity(TransformUsageFlags.None);
+            
+            AddComponent(entity, new SpawnCubeConfig
+            {
+                cubePrefabEntity = GetEntity(authoring.cubePrefab, TransformUsageFlags.Dynamic),
+                amountToSpawn = authoring.spawnAmount,
+            });
+        }
+    }
+}
+
+public struct SpawnCubeConfig : IComponentData
+{
+    public Entity cubePrefabEntity;
+    public int amountToSpawn;
+
+
+}
---
+++ Assets/SpawnCubeSystem.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Versioning;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public partial class SpawnCubeSystem : SystemBase
+{
+    protected override void OnCreate()
+    {
+        RequireForUpdate<SpawnCubeConfig>();
+    }
+    
+    protected override void OnUpdate()
+    {
+        //Ensures it only runs once
+        this.Enabled = false;
+
+        SpawnCubeConfig spawnCubeConfig = SystemAPI.GetSingleton<SpawnCubeConfig>();
+
+        for (int i = 0; i < spawnCubeConfig.amountToSpawn; i++)
+        {
+            Entity spawnedEntity = EntityManager.Instantiate(spawnCubeConfig.cubePrefabEntity);
+            EntityManager.SetComponentData(spawnedEntity, new LocalTransform
+            {
+                Position = new float3(Random.Range(-1000,500), 0.6f, Random.Range(-400,700)),
+                Rotation = quaternion.identity,
+                Scale = 1f
+            });
+        }
+    }
+}
