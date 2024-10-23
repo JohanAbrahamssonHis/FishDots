2f6c6543a4daae35f0a7218e4858924bf90506ce
JohanAbrahamssonHis <118519456+JohanAbrahamssonHis@users.noreply.github.com>
2024-10-23 11:43:09 +0200

    You found a video that might help, do it

--- Assets/Fish Dots/HandleFishSystem.cs
+++ Assets/Fish Dots/HandleFishSystem.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
 using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
@@ -14,20 +15,19 @@ public partial struct HandleFishSystem : ISystem
     
     public void OnUpdate(ref SystemState state)
     {
-        
-        
-       /* 
+
+        /*
+        List<FishJob> fishJobs = new List<FishJob>();
+       
         foreach (FishAspect fishAspect in
                  SystemAPI.Query<FishAspect>())
         {
             //fishAspect.FishLogic(SystemAPI.Time.DeltaTime);
             
+            
         }
         */
-
-       //CollectionHelper.CreateNativeArray<FishComponent>( SystemAPI.Query<FishComponent>, ref World.UpdateAllocator);
-       
-       
+        
         FishJob fishJob = new FishJob
         {
             deltaTime = SystemAPI.Time.DeltaTime,
@@ -36,10 +36,28 @@ public partial struct HandleFishSystem : ISystem
             //fish = fishAspect.fish.ValueRW,
             //localTransform = fishAspect.localTransform.ValueRW,
         };
+        
+        JobHandle job = fishJob.Schedule();
+        
+        fishJob.neighbourFish.Dispose(job);
+        
+
+       //CollectionHelper.CreateNativeArray<FishComponent>( SystemAPI.Query<FishComponent>, ref World.UpdateAllocator);
+
+       /*
+       FishJob fishJob = new FishJob
+        {
+            deltaTime = SystemAPI.Time.DeltaTime,
+            neighbourFish = new NativeList<LocalTransform>(Allocator.Persistent),
+            AllFish = new NativeList<LocalTransform>(Allocator.Persistent),
+            fish = fishAspect.First().fish.ValueRW,
+            localTransform = fishAspect.First().localTransform.ValueRW,
+        };
             
         JobHandle job = fishJob.Schedule();
             
         fishJob.neighbourFish.Dispose(job);
+        */
         
         //job.Complete();
 
